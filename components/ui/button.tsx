53980dc25a91e7b005d745228ef2e7b14e4815cb
250862-italia <g.innocenti@magnificusdominus.com>
2025-06-26 17:11:17 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,16 +1,12 @@
-'use client'
+// components/ui/button.tsx
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
-
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button
-      className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-      {...props}
-    >
+    <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700" {...props}>
       {children}
     </button>
-  )
+  );
 }
